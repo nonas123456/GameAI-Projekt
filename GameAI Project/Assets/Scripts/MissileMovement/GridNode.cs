8ab933fd4a8eabf4310aaa4ec31c959ef65bc345
Cowweb <jonas@profibermail.dk>
2026-04-07 11:05:04 +0200

    Made GrafClass.
    Created a graph as well as nodes for the graph with neighbor relationships.

---
+++ GameAI Project/Assets/Scripts/MissileMovement/GridNode.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridNode : MonoBehaviour
+{
+    public int gridX;
+    public int gridZ;
+    
+    public List<GridNode> neighbors = new List<GridNode>();
+
+    public GridNode(int x, int z)
+    {
+        gridX = x;
+        gridZ = z;
+    }
+    
+}
---
+++ GameAI Project/Assets/Scripts/MissileMovement/MissileMovement.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class MissileMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void LateUpdate()
+    {
+        throw new NotImplementedException();
+    }
+}
