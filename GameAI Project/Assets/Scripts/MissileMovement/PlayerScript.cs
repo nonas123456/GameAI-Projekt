61adeb9f3a1c476450ffe88088ec60577cda6414
Cowweb <jonas@profibermail.dk>
2026-04-28 11:40:15 +0200

    Added a lot of textures also made the player an ice skater or something ITS A FEATURE NOT A BUG :D

--- GameAI Project/Assets/Scripts/MissileMovement/PlayerScript.cs
+++ GameAI Project/Assets/Scripts/MissileMovement/PlayerScript.cs
@@ -4,6 +4,7 @@ public class PlayerScript : MonoBehaviour
 {
     public GameObject CurrentTile;
     private RaycastHit hit;
+    public Rigidbody rb;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,7 +15,35 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit);
+        Physics.Raycast(transform.position + new Vector3(2f, 0f, 0f), transform.TransformDirection(Vector3.down), out hit);
         CurrentTile = hit.collider.gameObject;
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            print("Pressed W");
+            rb.AddForce(transform.TransformDirection(Vector3.forward) * 10, ForceMode.Force);
+            print("Added Force");
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            print("Pressed S");
+            rb.AddForce(transform.TransformDirection(Vector3.back) * 10, ForceMode.Force);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Pressed A");
+            rb.AddForce(transform.TransformDirection(Vector3.left) * 10, ForceMode.Force);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("Pressed D");
+            rb.AddForce(transform.TransformDirection(Vector3.right) * 10, ForceMode.Force);
+        }
+
+
     }
 }
